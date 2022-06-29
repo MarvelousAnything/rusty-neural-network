5e3407fcf026c46434e95c54fc99b7c5d9e3f23b
Owen V. Hayes <marvelousanything@gmail.com>
2022-06-29 16:51:50 -0700

    time for another refactor

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,98 @@
-use std::ops::{Add, Neg};
+use std::ops::{Add, Div, Neg};
+use ndarray::{arr1, Array1, Array2};
+use ndarray_rand::RandomExt;
 use rand::{Rng, thread_rng};
-use rand::distributions::Standard;
+use rand::distributions::Uniform;
 
 fn sigmoid(x: f64) -> f64 {
-    x.neg().exp().add(1f64).powi(-1)
+    x.neg().exp().add(1.0).powi(-1)
 }
 
-#[derive(Debug)]
-struct Node {
-    weight: Vec<f64>,
-    bias: f64,
-}
-
-impl Node {
-    pub fn new(prev_nodes: usize) -> Node {
-        let weight: Vec<f64> = thread_rng().sample_iter(Standard).take(prev_nodes).collect();
-        let bias: f64 = thread_rng().gen_range(0.0..1.0);
-        Node { weight, bias }
-    }
+fn sigmoid_derivative(x: f64) -> f64 {
+    x.exp().div(x.exp().add(1.0).powi(2))
 }
 
 #[derive(Debug)]
 struct Layer {
-    nodes: Vec<Node>,
-    layer_size: usize
+    size: usize,
+    bias: f64,
+    weights: Array2<f64>
 }
 
 impl Layer {
-    pub fn new(layer_size: usize, prev_layer_size: usize) -> Layer {
-        let nodes: Vec<Node> = (0..layer_size).map(|_| Node::new(prev_layer_size)).collect();
-        Layer { nodes, layer_size }
+    pub fn new(size: usize, prev_size: usize) -> Layer {
+        let bias: f64 = thread_rng().gen_range(0.0..1.0);
+        let weights: Array2<f64> = Array2::random((prev_size, size), Uniform::new(0.0, 1.0));
+        Layer { size, bias, weights }
     }
 }
-
 
 #[derive(Debug)]
 struct Network {
-    sizes: Vec<usize>,
-    layers: Vec<Layer>,
-    num_layers: usize
+    topology: Vec<usize>,
+    topology_size: usize,
+    input_size: usize,
+    output_size: usize,
+    layers: Vec<Layer>
 }
 
 impl Network {
-    pub fn new(sizes: Vec<usize>) -> Network {
+    pub fn new(topology: Vec<usize>) -> Network {
         let mut layers: Vec<Layer> = Vec::new();
-        let mut prev_size: usize = 0;
-        for size in &sizes {
-            let layer: Layer = Layer::new(*size, prev_size);
-            prev_size = *size;
-            layers.push(layer);
+        let topology_size = topology.len();
+        for size in 0..topology_size-1 {
+            layers.push(Layer::new(topology[size + 1], topology[size]));
         }
-        let num_layers = sizes.len();
-        Network { sizes, layers, num_layers }
+        let input_size: usize = topology[0];
+        let output_size: usize = topology[topology.len() - 1];
+        Network { topology, topology_size, input_size, output_size, layers }
+    }
+
+    pub fn think(&self, input_data: Array1<f64>) -> Vec<Array1<f64>> {
+        let mut input: Array1<f64> = input_data;
+        let mut outputs: Vec<Array1<f64>> = Vec::new();
+        let mut counter = 0;
+        println!("{counter}\t{input:?}");
+        for layer in self.layers.iter() {
+            input = layer.weights.t().dot(&input) + layer.bias;
+            input = input.map(|x| sigmoid(*x));
+            outputs.push(input.clone());
+            counter += 1;
+            println!("{counter}\t{input:?}");
+        }
+        outputs
+    }
+
+    pub fn train(&self, training_inputs: Array1<f64>, training_outputs: Array1<f64>, epochs: usize) {
+        for _ in 0..epochs {
+            let outputs = self.think(training_inputs.clone());
+            let mut output_error: Array1<f64> = &training_outputs - &outputs[self.topology_size-2];
+
+            for i in (0..self.topology_size-1).rev() {
+                let out = &outputs[i];
+                let weights = &self.layers[i].weights;
+                let gradients = out.map(|x| sigmoid_derivative(*x)) * &output_error;
+
+                println!("\n{:-^20}", self.topology_size-3 + i);
+                println!("out: {:#?}", out);
+                println!("weights: {:#?}", weights);
+                println!("\ngradients: {gradients:#?}");
+                println!("\nerror: {output_error:#?}");
+                println!("\ntest: {:#?}", gradients.dot(&weights.t()));
+
+                output_error = outputs[i - 1].clone();
+            }
+
+            // training_inputs.clone().dot(&(output.map(|x| sigmoid_derivative(*x)) * error));
+        }
     }
 }
-
-/*
-struct NetworkBuilder {
-    inputs: usize,
-    outputs: usize,
-    hidden_layers: Vec<Layer>,
-}
-
-impl NetworkBuilder {
-    pub fn new(inputs: usize, outputs: usize) -> NetworkBuilder {
-        let hidden_layers: Vec<Layer> = Vec::new();
-        NetworkBuilder { inputs, outputs, hidden_layers }
-    }
-
-    pub fn add_layer(mut self, layer_size: usize) -> NetworkBuilder {
-        let layer = Layer::new(layer_size);
-        self.hidden_layers.push(layer);
-        self
-    }
-
-    pub fn build(self) -> Network {
-        let input_layer: Layer = Layer::new(self.inputs);
-        let output_layer: Layer = Layer::new(self.outputs);
-
-        Network { inputs: self.inputs, outputs: self.outputs, input_layer, hidden_layers: self.hidden_layers, output_layer }
-    }
-}
-*/
 
 fn main() {
-    let sizes = vec![2, 3, 1];
-    let network = Network::new(sizes);
-    println!("{:#?}", network)
-
-
+    let network = Network::new(vec![2, 4, 8, 4]);
+    let inputs: Array1<f64> = arr1(&[0.3, 0.2]);
+    let outputs: Array1<f64> = arr1(&[0.9, 0.3, 0.5, 0.6]);
+    // println!("{:#?}", network);
+    network.train(inputs, outputs, 1);
+    // println!("{:#?}", network.think(inputs));
 }
